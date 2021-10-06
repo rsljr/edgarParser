36db169129d747cc12fc15bb99c1f8a8ec71b0f0
Ronei Leonel <ronei.leonel@gmail.com>
2021-10-06 01:14:26 -0400

    update 10k

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# python-edgar #
+# edgarParser #
 
 > "EDGAR is the Electronic Data Gathering, Analysis, and Retrieval system used at the U.S. Securities and Exchange Commission (SEC). EDGAR is the primary system for submissions by companies and others who are required by law to file information with the SEC.  
 >
 > Containing millions of company and individual filings, EDGAR benefits investors, corporations, and the U.S. economy overall by increasing the efficiency, transparency, and fairness of the securities markets. The system processes about 3,000 filings per day, serves up 3,000 terabytes of data to the public annually, and accommodates 40,000 new filers per year on average." [About EDGAR](https://www.sec.gov/edgar/about)
 
-EDGAR is a rich source of data, but working with it is not straightforward. Although there are many packages to access and download company filings, parsing these filings is a different story.  
-  
-As part of my research projects, I coded some scripts to parse filings and extract the information I needed. I am making some of my scripts available to facilitate or inspire my fellow researchers facing similar problems.
+EDGAR is a rich source of data. There are many packages to access and download company filings. However, because working with filings is not straightforward, parsers for SEC filings are few and far between packages.
+
+As part of my research projects, I have written parsers to extract data from filings. I decided to share these parsers to democratize and ease the access to information that often is sold by thousands of dollars.
 
 ## 3, 4, and 5 form: Corporate insider data ##
 
@@ -20,12 +20,12 @@ The function [get_executive_compensation()](https://github.com/rsljr/python-edga
 
 The function [parse_8k_filing()](https://github.com/rsljr/python-edgar/blob/master/parse_8K.py) parses 8-K forms to extract disclosed items and their associated text.  You can check the page [Expansion of Form 8-K Items](https://www.sec.gov/rules/final/33-8400.htm) to see item meaning.  
 
-## 13f form" Institutional Investors ##
+## 13f form: Institutional Investors ##
 
 The function [parse_13f_filing()](https://github.com/rsljr/python-edgar/blob/master/parse_13f.py) parses 13f forms to extract data regarding institutional investors and their portfolios.
 
 ## 10-K form: Business, Risk, MD&A ##
 
-The function [parse_10k_filing()](https://github.com/rsljr/python-edgar/blob/master/parse_10K.py) parses 10-K forms to extract the sections business description, risk, and management discussion and analysis.  
+The function [parse_10k_filing()](https://github.com/rsljr/python-edgar/blob/master/parse_10K.py) parses 10-K forms to extract the sections: business description, risk, and management discussion and analysis.  
 
 Visit [Accessing EDGAR Data](https://www.sec.gov/edgar/searchedgar/accessing-edgar-data.htm) to know more about EDGAR.  
--- parse_10K.py
+++ parse_10K.py
@@ -3,135 +3,88 @@
 
 # # 10-K form
 # ## Business, Risk, and MD&A
-# The function *parse_10k_filing()* parses 10-K forms to extract the following sections: business description, business risk, and management discussioin and analysis. The function takes two arguments, a link and a string indicating the section. Current options are **"All", "Business", "Risk", "MDA."**
+# The function *parse_10k_filing()* parses 10-K forms to extract the following sections: business description, business risk, and management discussioin and analysis. The function takes two arguments, a link and a number indicating the section, and returns a list with the requested sections. Current options are **0(All), 1(Business), 2(Risk), 4(MDA).**
 # 
 # Caveats:
 # The function *parse_10k_filing()* is a parser. You need to feed a SEC text link into it. There are many python and r packages to get a direct link to the fillings.
 # 
 
-import pandas as pd
 import re
 import unicodedata
 from bs4 import BeautifulSoup as bs
 import requests
-import numpy as np
-import sys
 
 def parse_10k_filing(link, section):
     
-    if section not in ["All", "Business", "Risk", "MDA"]:
+    if section not in [0, 1, 2, 3]:
+        print("Not a valid section")
         sys.exit()
     
     def get_text(link):
-        page = requests.get(link)
+        page = requests.get(link, headers={'User-Agent': 'Mozilla'})
         html = bs(page.content, "lxml")
         text = html.get_text()
         text = unicodedata.normalize("NFKD", text).encode('ascii', 'ignore').decode('utf8')
         text = text.split("\n")
+        text = " ".join(text)
         return(text)
     
-    def extract_text(text, item, criteria):
+    def extract_text(text, item_start, item_end):
+        item_start = item_start
+        item_end = item_end
+        starts = [i.start() for i in item_start.finditer(text)]
+        ends = [i.start() for i in item_end.finditer(text)]
+        positions = list()
+        for s in starts:
             control = 0
-            n = 0
-            businessList = list()
-            tableContent = 0
-            for line in text:
-                n += 1
-                line = line.strip()
-                if item.search(line) is not None:
-                    if control == 0:
-                        n = 0
+            for e in ends:
+                if control == 0:
+                    if s < e:
                         control = 1
-                        businessList.append(line)
-                    elif control == 1:
-                        businessList = list()
-                        businessList.append(line)
-                        control = 2         
-                elif control == 1:
-                    if (criteria.search(line) is not None):
-                        # identifying if the first match occured in table of content
-                        if n > 100:
-                            if tableContent == 1:
-                                pass
-                            else:
-                                break
-                        if n < 30:
-                            tableContent = 1
+                        positions.append([s,e])
+        item_length = 0
+        item_position = list()
+        for p in positions:
+            if (p[1]-p[0]) > item_length:
+                item_length = p[1]-p[0]
+                item_position = p
 
-                    else:
-                        businessList.append(line)
+        item_text = text[item_position[0]:item_position[1]]
 
-                elif control == 2:
-                    if criteria.search(line) is not None:
-                        break
-                    else:
-                        businessList.append(line)
-            return(businessList)
-        
-    def extract_nameCIK(text):
-        companyCik = re.compile("(CENTRAL INDEX KEY:)([\s\d]+)", re.IGNORECASE)
-        companyName = re.compile("(COMPANY CONFORMED NAME:)(.+)", re.IGNORECASE)
-        control = 0
-        for line in text:
-            if companyName.search(line) is not None:
-                conm = companyName.search(line).group(2).strip()
-            if companyCik.search(line) is not None:
-                cik = companyCik.search(line).group(2).strip()
-                control == 1
-            if control == 1:
-                break
-        return(conm, cik)
+        return(item_text)
 
-    def get_business(text):
-        item1 = re.compile("^item\s*1\.*\s*\\b", re.IGNORECASE)
-        item1a = re.compile("^item\s*1a|^item\s*2", re.IGNORECASE)
-        businessList = extract_text(text, item1, item1a)
-        
-        return(businessList)
-    
-    def get_businessRisk(text):
-        item1a = re.compile("^item\s*1a\.*\s*\\b", re.IGNORECASE)
-        item2 = re.compile("^item\s*2", re.IGNORECASE)
-        riskList = extract_text(text, item1a, item2)
-        
-        return(riskList)
-    
-    def get_mda(text):
-        item7 = re.compile("^item\s*7[\.\:]\s*", re.IGNORECASE)
-        item7a = re.compile("^item\s*7a", re.IGNORECASE)
-        item8 = re.compile("^item\s*8", re.IGNORECASE)
-        is7a = any([item7a.search(line) for line in text])
-        if is7a == True:
-            mdaList = extract_text(text, item7, item7a)
-        else:
-            mdaList = extract_text(text, item7, item8)
-                            
-        return(mdaList)
-    
     text = get_text(link)
-    if section == "Business":
-        businessDes = get_business(text)
-        data = pd.DataFrame.from_dict([{"businessDescription": "\n".join(businessDes)}])
         
-    if section == "Risk":
-        riskDes = get_businessRisk(text)
-        data = pd.DataFrame.from_dict([{"riskDescription": "\n".join(riskDes)}])
+    if section == 1 or section == 0:
+        try:
+            item1_start = re.compile("item\s*[1][\.\;\:\-\_]*\s*\\b", re.IGNORECASE)
+            item1_end = re.compile("item\s*1a[\.\;\:\-\_]\s*Risk|item\s*2[\.\,\;\:\-\_]\s*Prop", re.IGNORECASE)
+            businessText = extract_text(text, item1_start, item1_end)
+        except:
+            businessText = "Something went wrong!"
         
-    if section == "MDA":
-        mdaDes = get_mda(text)
-        data = pd.DataFrame.from_dict([{"MDA": "\n".join(mdaDes)}])
-        
-    if  section == "All":
-        businessDes = get_business(text)
-        riskDes = get_businessRisk(text)
-        mdaDes = get_mda(text)
-        data = pd.DataFrame.from_dict([{"MDA": "\n".join(mdaDes),
-                                        "riskDescription": "\n".join(riskDes),
-                                       "businessDescription": "\n".join(businessDes)}])
-    ids = extract_nameCIK(text)
-    data["conm"] = ids[0]
-    data["CIK"] = ids[1]
-    data["edgar.link"] = link
-        
-        
+    if section == 2 or section == 0:
+        try:
+            item1a_start = re.compile("(?<!,\s)item\s*1a[\.\;\:\-\_]\s*Risk", re.IGNORECASE)
+            item1a_end = re.compile("item\s*2[\.\;\:\-\_]\s*Prop|item\s*[1][\.\;\:\-\_]*\s*\\b", re.IGNORECASE)
+            riskText = extract_text(text, item1a_start, item1a_end)
+        except:
+            riskText = "Something went wrong!"
+            
+    if section == 3 or section == 0:
+        try:
+            item7_start = re.compile("item\s*[7][\.\;\:\-\_]*\s*\\bM", re.IGNORECASE)
+            item7_end = re.compile("item\s*7a[\.\;\:\-\_]\sQuanti|item\s*8[\.\,\;\:\-\_]\s*", re.IGNORECASE)
+            mdaText = extract_text(text, item7_start, item7_end)
+        except:
+            mdaText = "Something went wrong!"
+    
+    if section == 0:
+        data = [businessText, riskText, mdaText]
+    elif section == 1:
+        data = [businessText]
+    elif section == 2:
+        data = [riskText]
+    elif section == 3:
+        data = [mdaText]
     return(data)
